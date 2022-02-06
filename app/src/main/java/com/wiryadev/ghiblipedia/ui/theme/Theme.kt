5deaac3d7f10c0130368a5e4f875867c025933f9
wiryadev <abrarwiryawan@gmail.com>
2022-02-06 19:23:59 +0700

    change theme

--- app/src/main/java/com/wiryadev/ghiblipedia/ui/theme/Theme.kt
+++ app/src/main/java/com/wiryadev/ghiblipedia/ui/theme/Theme.kt
@@ -6,16 +6,10 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = md_theme_light_primary,
+    primaryVariant = md_theme_light_primaryContainer,
+    secondary = md_theme_light_secondary
 
     /* Other default colors to override
     background = Color.White,
@@ -29,14 +23,8 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun GhiblipediaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
